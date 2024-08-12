4bc51d14b7de7f243e58a91c117e4c61326ad844
Nathan <59515478+ncamarena57@users.noreply.github.com>
2024-08-12 11:52:51 -0700

    Add final user story and bonus story

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import { marked } from 'marked';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
 
   return (
     <div className="App">
