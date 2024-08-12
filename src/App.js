d412706aea6c39277e1435ac82daeaceaaa22f41
Nathan <59515478+ncamarena57@users.noreply.github.com>
2024-08-12 11:35:48 -0700

    Add user story #3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
-      <textarea id='editor'></textarea>
-      <div id='preview'></div>
+      <textarea id='editor' onChange={(event) => {
+          setText(event.target.value)
+        }}
+        value={text}
+        ></textarea>
+      <div id='preview'>{text}</div>
     </div>
   );
 }
